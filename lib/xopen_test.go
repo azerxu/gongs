0ec9b76b899b475af665c644caa3bd978d0e29f8
azer <azer.xu@gmail.com>
2015-06-02 15:16:34 +0800

    update xopen_test

--- lib/xopen_test.go
+++ lib/xopen_test.go
@@ -8,7 +8,7 @@ import (
 var test_string = "this is xopen test string"
 var test_filename = "test_xopen"
 
-func Test_Xopen_txtfile(t *testing.T) {
+func Test_Xopen_txtfile_w(t *testing.T) {
 	filename := test_filename + ".txt"
 	w, err := Xcreate(filename, "w")
 	if err != nil {
@@ -37,6 +37,43 @@ func Test_Xopen_txtfile(t *testing.T) {
 	}
 	r.Close()
 	if err := os.Remove(filename); err != nil {
+		t.Fail()
+	}
+}
+
+func Test_Xopen_txtfile_a(t *testing.T) {
+	filename := test_filename + ".txt"
+	w, err := Xcreate(filename, "a")
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+	}
+
+	n, err := w.Write([]byte(test_string))
+	if n != len(test_string) || err != nil {
+		t.Fail()
+	}
+	w.Close()
+
+	r, err := Xopen(filename)
+	if err != nil {
+		t.Fail()
+	}
+	data := make([]byte, 4096)
+	n, err = r.Read(data)
+	if err != nil || n != len(test_string) {
+		t.Log(err)
+		t.Fail()
+	}
+	for i := 0; i < n; i++ {
+		if data[i] != test_string[i] {
+			t.Log(err)
+			t.Fail()
+		}
+	}
+	r.Close()
+	if err := os.Remove(filename); err != nil {
+		t.Log(err)
 		t.Fail()
 	}
 }
@@ -77,6 +114,54 @@ func Test_Xopen_gzfile(t *testing.T) {
 func Test_Xopen_file_not_exists(t *testing.T) {
 	filename := test_filename
 	_, err := Xopen(filename)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func Test_xopen_file_stdin(t *testing.T) {
+	f, err := Xopen("-")
+	if err != nil {
+		t.Fail()
+	}
+	// checks if we are getting data from stdin.
+	if f.(*os.File).Fd() != 0 {
+		t.Fail()
+	}
+}
+
+func Test_Xcreate_file_stdout_w(t *testing.T) {
+	f, err := Xcreate("-", "w")
+	if err != nil {
+		t.Fail()
+	}
+	if f.(*os.File).Fd() != 1 {
+		t.Fail()
+	}
+}
+
+func Test_Xcreate_file_stdout_a(t *testing.T) {
+	f, err := Xcreate("-", "a")
+	if err != nil {
+		t.Fail()
+	}
+	if f.(*os.File).Fd() != 1 {
+		t.Fail()
+	}
+}
+
+func Test_Xcreate_file_stderr(t *testing.T) {
+	f, err := Xcreate("@", "w")
+	if err != nil {
+		t.Fail()
+	}
+	if f.(*os.File).Fd() != 2 {
+		t.Fail()
+	}
+}
+
+func Test_Xcreate_cant_create_file(t *testing.T) {
+	_, err := Xcreate("/xopen")
 	if err == nil {
 		t.Fail()
 	}
