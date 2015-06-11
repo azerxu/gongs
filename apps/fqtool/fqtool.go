411b8aee6df28a9da8cb8bf57920032c4dbc60f3
azer <azer.xu@gmail.com>
2015-06-11 17:38:28 +0800

    update fqtool.go

--- apps/fqtool/fqtool.go
+++ apps/fqtool/fqtool.go
@@ -11,6 +11,15 @@ const mainVersion = "2015.06.11.1"
 
 func main() {
 	cmd := command.New(mainName, mainDesc, mainVersion)
-	cmd.Add(&command.SubCommand{Name: countName, Desc: countDesc, Usage: countArger.Usage, Runner: countRunner})
+	cmd.Add(&command.SubCommand{ // add count command
+		Name:   countName,
+		Desc:   countDesc,
+		Usage:  countArger.Usage,
+		Runner: countRunner})
+	cmd.Add(&command.SubCommand{ // add sample command
+		Name:   sampleName,
+		Desc:   countDesc,
+		Usage:  sampleArger.Usage,
+		Runner: sampleRunner})
 	cmd.Run(os.Args[1:]...)
 }
