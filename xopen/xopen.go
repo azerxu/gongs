ae1256a099786bb09b6de1ff4a830be4e7a139e6
azer <azer.xu@gmail.com>
2015-08-14 15:20:36 +0800

    add xopen.go
    xopen auto read from stdin, raw file, gzip file, bzip file, url link

--- xopen/xopen.go
+++ xopen/xopen.go
@@ -1,35 +1,71 @@
-package lib
+// auto read from stdin, gzip, bzip, raw or url
+// auto write data to stdout, stderr, gzip or raw
+
+package xopen
 
 import (
+	"compress/bzip2"
 	"compress/gzip"
+	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"strings"
 )
 
+// BzipReadCloser add a Close function for bzip2
+type BzipReadCloser struct {
+	r    io.Reader
+	file *os.File
+}
+
+// Read Using bzip2 Reader's Read method
+func (bz *BzipReadCloser) Read(buf []byte) (int, error) {
+	return bz.r.Read(buf)
+}
+
+// Close Close BzipReadCloser
+func (bz *BzipReadCloser) Close() error {
+	return bz.file.Close()
+}
+
+// Xopen read from stdin, raw, gzip, bzip2 or url
 func Xopen(filename string) (io.ReadCloser, error) {
-	if filename == "-" {
+	if filename == "-" { // check input is stdin or not
 		return os.Stdin, nil
+	}
+
+	// check input from an url string or not
+	if strings.HasPrefix(filename, "http://") || strings.HasPrefix(filename, "https://") {
+		r, err := http.Get(filename)
+		if err != nil {
+			return nil, err
+		}
+		if r.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("http error while loading %v. status: %v", filename, r.Status)
+		}
+		return r.Body, nil
 	}
 
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
+	if strings.HasSuffix(filename, ".bz2") || strings.HasSuffix(filename, ".bz") {
+		return &BzipReadCloser{r: bzip2.NewReader(file), file: file}, nil
+	}
 
-	gfile, gerr := gzip.NewReader(file)
-	if gerr == nil {
+	if gfile, err := gzip.NewReader(file); err == nil {
 		return gfile, nil
 	}
 
-	_, err = file.Seek(0, 0)
-	if err != nil {
+	if _, err = file.Seek(0, 0); err != nil {
 		return nil, err
 	}
-
 	return file, nil
 }
 
+// Xcreate write data to stdout, stderr, file or gzip file
 func Xcreate(args ...string) (io.WriteCloser, error) {
 	filename := "-"
 	mode := "w"
