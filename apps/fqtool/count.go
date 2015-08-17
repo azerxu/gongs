0548dfb7cda8bb8af863e7c0208d58537ff6a4ed
azer <azer.xu@gmail.com>
2015-08-17 20:25:08 +0800

    update count.go

--- apps/fqtool/count.go
+++ apps/fqtool/count.go
@@ -2,32 +2,59 @@ package main
 
 import (
 	"fmt"
-	"gongs/arger"
+	"gongs/argparser"
 	"gongs/biofile/fastq"
 	"os"
+	"runtime"
+	"sync"
 )
 
 const countName = "count"
 const countDesc = "count reads from fastq file"
 
-var countArger = arger.New(mainName, countName)
+var countArger = argparser.New(mainName, countName)
 
-func countRunner(args ...string) {
-	if len(args) == 0 {
+func countRunner(filenames ...string) {
+	if len(filenames) == 0 {
 		fmt.Fprintln(os.Stderr, mainName, countName, ": no input given!")
 		os.Exit(1)
 	}
-	for _, file := range args {
-		fmt.Println("file:", file)
-		fqfile, err := fastq.Open(file)
+
+	// setting multi-threads
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	fqfiles := make([]*fastq.FastqFile, len(filenames))
+	for i, filename := range filenames {
+		fqfile, err := fastq.Open(filename)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		cc := 0
-		for _ = range fqfile.Load() {
-			cc++
-		}
-		fmt.Println(file, "contain Reads:", cc)
+		fqfiles[i] = fqfile
 	}
+
+	chCount := make(chan int, len(filenames))
+	go func(chCount chan int, fqfiles []*fastq.FastqFile) {
+		wg := &sync.WaitGroup{}
+		for _, fqfile := range fqfiles {
+			wg.Add(1)
+			go func(wg *sync.WaitGroup, fqfile *fastq.FastqFile, chCount chan int) {
+				count := 0
+				for fqfile.Next() {
+					count++
+				}
+				chCount <- count
+				fmt.Println(fqfile.Name, "contain Reads:", count)
+				wg.Done()
+			}(wg, fqfile, chCount)
+		}
+		wg.Wait()
+		close(chCount)
+	}(chCount, fqfiles)
+
+	var tot int
+	for c := range chCount {
+		tot += c
+	}
+	fmt.Println("Total Reads:", tot)
 }
