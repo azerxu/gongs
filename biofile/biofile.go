99ba844f44bc635d726ab564b9f568da5d1cd77a
azer <azer.xu@gmail.com>
2015-08-25 18:24:43 +0800

    update biofile.go

--- biofile/biofile.go
+++ biofile/biofile.go
@@ -11,3 +11,28 @@ type SeqIter interface {
 	Next() bool
 	Value() (string, []byte, []byte)
 }
+
+type SeqFiler interface {
+	SeqIter
+	Seqs() <-chan Seqer
+	Close() error
+	Err() error
+}
+
+type PairSeqer interface {
+	GetRead1() Seqer
+	GetRead2() Seqer
+	String() string
+}
+
+type PairSeqIter interface {
+	Next() bool
+	Value() (Seqer, Seqer)
+}
+
+type PairSeqFiler interface {
+	PairSeqIter
+	Pairs() <-chan PairSeqer
+	Close() error
+	Err() error
+}
